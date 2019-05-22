fc454cfba3c6c45258b8969e1fe4997096c95804
Duruhan Unsal <duruhanunsal@gmail.com>
2019-05-22 15:05:52 -0400

    Add fs

--- rest-api/src/index.ts
+++ rest-api/src/index.ts
@@ -2,6 +2,10 @@ import { ApiCall } from './api-call';
 import { User } from './User';
 import { Repo } from './Repo';
 
+import fs from 'fs';
+const append = fs.appendFile;
+const write = fs.writeFile;
+
 // We collect the username from command line
 let username = process.argv[2];
 // We instantiate a request server here.
@@ -12,5 +16,18 @@ let service = new ApiCall();
 // are seperate but they can get further integrated and work in sync
 // In this case, there wouldn't be two API calls but one. Do keep in
 // mind that you can come close to callback hell!
-service.getUserInfo(username, (user: User) => console.log(user));
-service.getRepos(username, (repos: Repo[]) => console.log(repos));
+service.getUserInfo(username, (user: User) => {
+  write(`./${username}.txt`, JSON.stringify(user), err => {
+    if (err) console.log(err);
+    console.log('Successfully written User to file.');
+  });
+});
+
+service.getRepos(username, (repos: Repo[]) => {
+  append(`./${username}.txt`, JSON.stringify(repos), err => {
+    if (err) console.log(err);
+    console.log('Successfully written Repos to file.');
+  });
+});
+
+export default service;
